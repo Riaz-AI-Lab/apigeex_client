205d9f27b94c95521843713115f1b0763336ff65
tranngocphu <tr.ngocphu@gmail.com>
2025-07-24 09:57:15 -0700

    add helper function in readme

--- examples/openai_chat.py
+++ examples/openai_chat.py
@@ -1,40 +1,51 @@
 import json
-
 from dotenv import load_dotenv
 load_dotenv()
 
+# import the API wrapper, this is standard to any API
+from apigeex_client.wrapper import wrapper
 
-if __name__ == "__main__":
+# import relevant functions from OpenAI API
+from apigeex_client.clients.llm_azure_openai.api.default import chat_completions_create
+from apigeex_client.clients.llm_azure_openai.models.create_chat_completion_request import CreateChatCompletionRequest
+from apigeex_client.clients.llm_azure_openai.models.chat_completion_request_message import ChatCompletionRequestMessage
+from apigeex_client.clients.llm_azure_openai.models.chat_completion_request_message_role import ChatCompletionRequestMessageRole
 
-    # import the API wrapper, this is standard to any API
-    from apigeex_client.wrapper import wrapper
-    
-    # import relevant functions from OpenAI API
-    from apigeex_client.clients.llm_azure_openai.api.default import chat_completions_create
-    from apigeex_client.clients.llm_azure_openai.models.create_chat_completion_request import CreateChatCompletionRequest
-    from apigeex_client.clients.llm_azure_openai.models.chat_completion_request_message import ChatCompletionRequestMessage
-    from apigeex_client.clients.llm_azure_openai.models.chat_completion_request_message_role import ChatCompletionRequestMessageRole
 
-    # Create a OpenAI API client from the wrapper
-    client = wrapper.get_client("llm_azure_openai")
+# Create a OpenAI API client from the wrapper
+client = wrapper.get_client("llm_azure_openai")
+API_VERSION = '2024-10-21'
 
-    # Create system and user prompt objects
-    context = "You are an AI assistant that helps doctors find medical information."
-    prompt = "What are the prescription guidelines for Methotrexate?"   
-    system_prompt = ChatCompletionRequestMessage( role=ChatCompletionRequestMessageRole.SYSTEM, content=context )
-    message = ChatCompletionRequestMessage( role=ChatCompletionRequestMessageRole.USER, content=prompt )    
-    
+
+# Helper function to quickly query the LLM and get the answer.
+def query_llm(user_prompt: str, system_prompt: str, engine: str = 'gpt-4'):
+
+    system_prompt = ChatCompletionRequestMessage( role=ChatCompletionRequestMessageRole.SYSTEM, content=system_prompt )
+    message = ChatCompletionRequestMessage( role=ChatCompletionRequestMessageRole.USER, content=user_prompt )   
+    completion_request_body = CreateChatCompletionRequest( messages=[ system_prompt, message ] ) 
+
     response = chat_completions_create.sync_detailed(
-        deployment_id="gpt-4o",
+        deployment_id=engine,
         client=client,
-        body=CreateChatCompletionRequest( messages=[ system_prompt, message ] ),
-        api_version='2024-10-21',
+        body=completion_request_body,
+        api_version=API_VERSION,
     )
 
     if response.status_code == 200:
         data_dict = response.parsed.to_dict()
-        json_str = json.dumps(data_dict, indent=2)
-        print(json_str)
+        return data_dict['choices'][0]['message']['content']
     else:
         print("ERROR:", response.status_code)
         print(response)
+        return None
+
+
+
+if __name__ == "__main__":
+
+    context = "You are an AI assistant that helps doctors find medical information."
+    prompt = "What are the prescription guidelines for Methotrexate?" 
+
+    reply = query_llm(prompt, context, engine='gpt-4')
+
+    print(reply)
